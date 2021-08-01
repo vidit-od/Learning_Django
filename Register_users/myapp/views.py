336056fcf8249eb3e58b753bb5bfa5dd644b7ddf
vidit-od <vidit894@gmail.com>
2021-08-01 18:18:35 +0530

    Register_Users

--- Register_users/myapp/views.py
+++ Register_users/myapp/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from .models import person
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    persons=person.objects.all()
+    return render(request, 'index.html',{'persons':persons})
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        Email=request.POST['email']
+        password=request.POST['password']
+        re_password=request.POST['re_password']
+
+        if password == re_password:
+            if User.objects.filter(email=Email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('login')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exist')
+                return redirect('login')
+            else:
+                user=User.objects.create(username=username,email=Email,password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'password do not match')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+            
+    return render(request, 'login.html')
