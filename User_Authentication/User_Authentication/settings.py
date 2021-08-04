7c5381b2832b8af75662fd70d17e483242a21fa3
vidit-od <vidit894@gmail.com>
2021-08-04 11:18:19 +0530

    postgred connection

--- User_Authentication/User_Authentication/settings.py
+++ User_Authentication/User_Authentication/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'User_Authentication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'Project',
+         'USER':'postgres',
+         'PASSWORD':'vidit894',
+         'HOST':'localhost'
+     }
 }
 
 
